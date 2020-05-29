a7c7990bd34bcf1479343289e0516eb4ad1c61c0
emacslisp <java.gdb@gmail.com>
2020-05-30 01:04:54 +1000

    add basic usage of lib.ps1

--- lib.ps1
+++ lib.ps1
@@ -3,3 +3,20 @@ function ShowMessage($AValue)
   $a = new-object -comobject wscript.shell
   $b = $a.popup( $AValue )
 }
+
+
+function isLeapYear($year) 
+{
+  return [DateTime]::IsLeapYear($year);
+}
+
+function now
+{
+  Write-Output "DateTime now is:"
+  return [DateTime]::Now;
+}
+
+function totalDay($yearStart, $yearEnd) {
+  $result = [DateTime] $yearEnd - [DateTime] $yearStart;
+  return $result.TotalDays;
+}
